43310c4bb6ff29b7db9d660f0f4ab3115197f5ff
joyce <joycedelatorre87@gmail.com>
2018-07-03 23:16:50 -0500

    this maybe a possible direction for creating tooltip through TD stashing this version only

--- stocks.js
+++ stocks.js
@@ -14,24 +14,33 @@ dataSet = [
 
 $(document).ready(function(){
 
+// first sort the dataset to achieve the descending order.
 dataSet.sort(function (a, b){
 	return parseFloat(b.marketCap) - parseFloat(a.marketCap);
 });
 //console.log(dataSet); //--> to check sorted dataset
+// once sorted call the renderChart() to display the chart.
 
 function renderChart(){
 	for (var i = 0; i < dataSet.length; i++){
 		// console.log(dataSet[i].marketCap);
 		$(".table-svg-chart").append("<tr id="+ i +
 			"><td><input type='checkbox' id='"+ i +"'></td><td>"+ dataSet[i].name +
-			"</td><td><figure><svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' class='chart' width='1000' height='19' aria-labelledby='title' role='img'><g class='bar'><rect width='"+dataSet[i].marketCap+"' height='19'></rect></g></figure></td>"
+			"</td><td><figure><svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' class='chart' width='1000' height='19' aria-labelledby='title' role='img'><g class='bar'><rect width='"+dataSet[i].marketCap+"' height='19'></rect></g></figure></td><td class='popUp'><span class='popuptext'>Price: "+dataSet[i].price+" MarketCap: "+dataSet[i].marketCap+"</span></td>"
 		);
 	}
 }
 
 
-// $("p").click(function(){
-//     alert("The paragraph was clicked.");
+$( "tr" ).hover(
+  function() {
+    $( this ).append( $( "<span> *** </span>" ) );
+  }
+);
+ 
+// $( "td.fade" ).hover(function() {
+//   $( this ).fadeOut( 100 );
+//   $( this ).fadeIn( 500 );
 // });
 
 renderChart();
